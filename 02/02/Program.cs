5e6e56dbae0b9a8dee06e4149c8883fda027bae9
ScoredSleet <matheus.vms11@gmail.com>
2025-04-26 22:27:26 -0300

    Adicionando arquivos do Visual Studio e comecando a programar eno Unity

---
+++ 02/02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        /// <summary> 
+        /// A class to print Hello World
+        /// </summary> 
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World\n");
+        }
+    }
+}
